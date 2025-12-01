26515a559c7b55b5ae86f3d5f113a3a21bb920ec
Gilberto <betho6990@gmail.com>
2025-11-30 23:14:26 -0600

    client page design

--- ventas-front/src/pages/Clientes.js
+++ ventas-front/src/pages/Clientes.js
@@ -2,11 +2,43 @@ import React from 'react'
 import Layout from '../components/commons/Layout';
 
 const Clientes = () => {
-    return ( 
+    return (
         <Layout>
-            <p>Clientes</p>
+            <div className='panel '>
+                <div className='panel-heading'>
+                    Clientes
+                </div>
+                <div className='box'>
+                    <table className='table is-hoverable is-fullwidth'>
+                        <thead>
+                            <tr>
+                                <th>Nombres</th>
+                                <th>Apellidos</th>
+                                <th>Dirección</th>
+                                <th>Teléfono</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <button className='button'>
+                                        <span className='icon is-small'>
+                                            <i className='fas fa-bold'></i>
+                                        </span>
+                                    </button>
+                                </td>
+                                <td>Aguilar</td>
+                                <td>C. Lincon</td>
+                                <td>2222222221</td>
+                                <td>freddy@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </Layout>
-     );
+    );
 }
- 
+
 export default Clientes;
