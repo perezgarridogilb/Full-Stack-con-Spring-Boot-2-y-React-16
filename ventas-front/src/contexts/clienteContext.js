30cf7fe09feff3bdb5a76958a697f63f461232b2
Gilberto Pérez <betho6990@gmail.com>
2025-12-28 21:36:34 -0600

    ident code

--- ventas-front/src/contexts/clienteContext.js
+++ ventas-front/src/contexts/clienteContext.js
@@ -18,19 +18,19 @@ export const ClienteContextProvider = (props) => {
 
     const [state, dispatch] = useReducer(clienteReducer, initialState);
 
-        const obtenerClientes = async () => {
-// const response1 = [];
-            try {
-         const response = await Axios.get('/clientes');
-        console.log(response.data);
-                    dispatch({
+    const obtenerClientes = async () => {
+        // const response1 = [];
+        try {
+            const response = await Axios.get('/clientes');
+            console.log(response.data);
+            dispatch({
                 type: OBTENER_CLIENTES,
                 payload: response.data
             })
-    } catch (error) {
-        console.error(error);
-    }
-    
+        } catch (error) {
+            console.error(error);
+        }
+
         const clientes = [
             {
                 "idCliente": 1,
@@ -49,58 +49,58 @@ export const ClienteContextProvider = (props) => {
                 "email": "juan@gmail.com"
             }
         ];
-    
 
+
+    }
+
+    const registrarCliente = async cliente => {
+        try {
+            const response = await Axios.post('/clientes', cliente);
+
+            dispatch({
+                type: REGISTRAR_CLIENTE,
+                payload: response.data
+            });
+
+            return true; // ✅ éxito
+        } catch (error) {
+            console.error(error);
+            return false; // ❌ error
         }
+    };
 
-const registrarCliente = async cliente => {
-  try {
-    const response = await Axios.post('/clientes', cliente);
-
-    dispatch({
-      type: REGISTRAR_CLIENTE,
-      payload: response.data
-    });
-
-    return true; // ✅ éxito
-  } catch (error) {
-    console.error(error);
-    return false; // ❌ error
-  }
-};
-
-  const obtenerCliente = async (cliente) => {
-                try {
-                    const clienteEncontrado = null;
-                    if (cliente !== null) {
-                    const response = await Axios.get(`/clientes/${cliente.idCliente}`);
-                        clienteEncontrado = response.data;
-                    } else {
-                        clienteEncontrado = cliente;
-                    }
+    const obtenerCliente = async (cliente) => {
+        try {
+            const clienteEncontrado = null;
+            if (cliente !== null) {
+                const response = await Axios.get(`/clientes/${cliente.idCliente}`);
+                clienteEncontrado = response.data;
+            } else {
+                clienteEncontrado = cliente;
+            }
             dispatch({
                 type: OBTENER_CLIENTE,
                 payload: clienteEncontrado
             })
-    } catch (error) {
-        console.error(error);
-    }            
-
+        } catch (error) {
+            console.error(error);
         }
 
-        const actualizarCliente = cliente => {
-            dispatch({
-                type: MODIFICAR_CLIENTE,
-                payload: cliente
-            })
-        }
+    }
 
-                const eliminarCliente = idCliente => {
-            dispatch({
-                type: ELIMINAR_CLIENTE,
-                payload: idCliente
-            })
-        }
+    const actualizarCliente = cliente => {
+        dispatch({
+            type: MODIFICAR_CLIENTE,
+            payload: cliente
+        })
+    }
+
+    const eliminarCliente = idCliente => {
+        dispatch({
+            type: ELIMINAR_CLIENTE,
+            payload: idCliente
+        })
+    }
 
     return (
         <ClienteContext.Provider
